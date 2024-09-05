3347425d71b6f714bc2fe753589c8fa2cc53bb90
wisal <benchellouiawisal@gmail>
2024-09-05 11:51:31 +0200

    hello

---
+++ app.py
@@ -0,0 +1,19 @@
+#Try 1
+
+a = int(input("1,1: "))
+b = int(input("1,2: "))
+c = int(input("1,3: "))
+d = int(input("2,1: "))
+e = int(input("2,2: "))
+f = int(input("2,3: "))
+g = int(input("3,1: "))
+h = int(input("3,2: "))
+j = int(input("3,3: "))
+matrix =[ [a,b,c],
+            [d,e,f],
+            [g,h,j]]
+while 1:
+    x = int(input("column: "))
+    y = int(input("row: "))
+    print(matrix[x][y])
+    print('_'*10)
